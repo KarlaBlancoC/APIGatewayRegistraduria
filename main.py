19e84fd6114548eb959f4b1a3cc28070ac46c98e
KarlaBlancoC <krblanco0403@gmail.com>
2022-11-12 17:16:07 -0500

    api

--- main.py
+++ main.py
@@ -4,14 +4,40 @@ from flask import request
 from flask_cors import CORS
 import json
 from waitress import serve
+import requests
+import datetime
+
+from flask_jwt_extended import create_access_token, verify_jwt_in_request
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
 
 app = Flask(__name__)
 cors = CORS(app)
+app.config["JWT_SECRET_KEY"] = "Clave-secreta-123"
 
 
 def load_file_config():
     with open("config.json") as f:
         return json.load(f)
+
+
+@app.route("/login", methods=["POST"])
+def create_token():
+    data = request.get_json()
+    config_data = load_file_config()
+    url = config_data["url-backend-security"] + "/usuarios/validate"
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+
+    response = requests.post(url, json=data, headers=headers)
+    print("Prueba")
+    print(response)
+    if response.status_code == 200:
+        user = response.json()
+        expires = datetime.timedelta(seconds=60 * 60 * 24)
+        token = create_access_token(identity=user, expires_delta=expires)
+        return jsonify({"token": token, "user_id": user["_id"]})
+    else:
+        return jsonify({"msg": "Usuario o contraseña incorrecta"}), response.status_code
 
 
 @app.route("/", methods=["GET"])

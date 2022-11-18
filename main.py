a9628b749181dcd8ca41b47006aef5d4917a6c67
KarlaBlancoC <krblanco0403@gmail.com>
2022-11-17 20:30:22 -0500

    Implementacion de middleware

--- main.py
+++ main.py
@@ -6,19 +6,62 @@ import json
 from waitress import serve
 import requests
 import datetime
+import re
 
 from flask_jwt_extended import create_access_token, verify_jwt_in_request
+from flask_jwt_extended import JWTManager
 from flask_jwt_extended import get_jwt_identity
 from flask_jwt_extended import jwt_required
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config["JWT_SECRET_KEY"] = "Clave-secreta-123"
+jtw = JWTManager(app)
 
 
 def load_file_config():
     with open("config.json") as f:
         return json.load(f)
+
+
+@app.before_request
+def before_request_callback():
+    url = limpiar_url(request.path)
+    excluded_routes = ["/login"]
+    if url in excluded_routes:
+        print("Ruta Excluida del middleware", url)
+    else:
+        if verify_jwt_in_request():
+            usuario = get_jwt_identity()
+            rol = usuario["rol"]
+            if rol is not None:
+                if not validar_permiso(url, request.method.upper(), rol["_id"]):
+                    return jsonify({"message": "Permission denied"}), 401
+            else:
+                return jsonify({"message": "Permission denied"}), 401
+        else:
+            return jsonify({"message": "Permission denied"}), 401
+
+
+def limpiar_url(url):
+    partes = url.split("/")
+    for p in partes:
+        if re.search("\\d", p):
+            url = url.replace(p, "?")
+
+    return url
+
+
+def validar_permiso(url, metodo, id_rol):
+    config_data = load_file_config()
+    url_seguridad = config_data["url-backend-security"] + "/permisos-roles/validar-permiso/rol/" + id_rol
+    headers = {"Content-Type": "application/json; charset=utf-8"}
+    body = {
+        "url": url,
+        "metodo": metodo
+    }
+    response = requests.post(url_seguridad, headers=headers, json=body)
+    return response.status_code == 200
 
 
 @app.route("/login", methods=["POST"])
@@ -27,10 +70,7 @@ def create_token():
     config_data = load_file_config()
     url = config_data["url-backend-security"] + "/usuarios/validate"
     headers = {"Content-Type": "application/json; charset=utf-8"}
-
     response = requests.post(url, json=data, headers=headers)
-    print("Prueba")
-    print(response)
     if response.status_code == 200:
         user = response.json()
         expires = datetime.timedelta(seconds=60 * 60 * 24)
@@ -40,10 +80,169 @@ def create_token():
         return jsonify({"msg": "Usuario o contraseña incorrecta"}), response.status_code
 
 
-@app.route("/", methods=["GET"])
-def test():
-    data = {"message": "Servidor del API Gateway corriendo"}
-    return jsonify(data)
+# Servicios para PARTIDOS
+
+@app.route("/partidos", methods=["GET"])
+def listar_partidos():
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/partidos"
+    response = requests.get(url)
+    return jsonify(response.json())
+
+
+@app.route("/partido/<string:id>", methods=["GET"])
+def mostrar_partido(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/partido/" + id
+    response = requests.get(url)
+    return jsonify(response.json())
+
+
+@app.route("/partido", methods=["POST"])
+def crear_partido():
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/partido"
+    info_partido = request.get_json()
+    response = requests.post(url, json = info_partido)
+    return jsonify(response.json())
+
+@app.route("/partido/<string:id>", methods=["PUT"])
+def actualizar_partido(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/partido/" + id
+    info_partido = request.get_json()
+    response = requests.put(url, json = info_partido)
+    return jsonify(response.json())
+
+@app.route("/partido/<string:id>", methods=["DELETE"])
+def eliminar_partido(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/partido/" + id
+    response = requests.delete(url)
+    return jsonify(response.json())
+
+
+# Servicios para MESAS
+@app.route("/mesas", methods=["GET"])
+def listar_mesas():
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/mesas"
+    response = requests.get(url)
+    return jsonify(response.json())
+
+
+@app.route("/mesa/<string:id>", methods=["GET"])
+def mostrar_mesa(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/mesa/" + id
+    response = requests.get(url)
+    return jsonify(response.json())
+
+
+@app.route("/mesa", methods=["POST"])
+def crear_mesa():
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/mesa"
+    info_mesa = request.get_json()
+    response = requests.post(url, json = info_mesa)
+    return jsonify(response.json())
+
+@app.route("/mesa/<string:id>", methods=["PUT"])
+def actualizar_mesa(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/mesa/" + id
+    info_mesa = request.get_json()
+    response = requests.put(url, json = info_mesa)
+    return jsonify(response.json())
+
+@app.route("/mesa/<string:id>", methods=["DELETE"])
+def eliminar_mesa(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/mesa/" + id
+    response = requests.delete(url)
+    return jsonify(response.json())
+
+
+# Servicios para CANDIDATOS
+@app.route("/candidatos", methods=["GET"])
+def listar_candidatos():
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/candidatos"
+    response = requests.get(url)
+    return jsonify(response.json())
+
+
+@app.route("/candidato/<string:id>", methods=["GET"])
+def mostrar_candidato(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/candidato/" + id
+    response = requests.get(url)
+    return jsonify(response.json())
+
+
+@app.route("/candidato", methods=["POST"])
+def crear_candidato():
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/candidato"
+    info_candidato = request.get_json()
+    response = requests.post(url, json = info_candidato)
+    return jsonify(response.json())
+
+@app.route("/candidato/<string:id>", methods=["PUT"])
+def actualizar_candidato(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/candidato/" + id
+    info_candidato = request.get_json()
+    response = requests.put(url, json = info_candidato)
+    return jsonify(response.json())
+
+@app.route("/candidato/<string:id>", methods=["DELETE"])
+def eliminar_candidato(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/candidato/" + id
+    response = requests.delete(url)
+    return jsonify(response.json())
+
+
+# Servicios para RESULTADOS
+@app.route("/resultados", methods=["GET"])
+def listar_resultados():
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/resultados"
+    response = requests.get(url)
+    return jsonify(response.json())
+
+
+@app.route("/resultado/<string:id>", methods=["GET"])
+def mostrar_resultado(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/resultado/" + id
+    response = requests.get(url)
+    return jsonify(response.json())
+
+
+@app.route("/resultado", methods=["POST"])
+def crear_resultado():
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/resultado"
+    info_resultado = request.get_json()
+    response = requests.post(url, json = info_resultado)
+    return jsonify(response.json())
+
+@app.route("/resultado/<string:id>", methods=["PUT"])
+def actualizar_resultado(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/resultado/" + id
+    info_resultado = request.get_json()
+    response = requests.put(url, json = info_resultado)
+    return jsonify(response.json())
+
+@app.route("/resultado/<string:id>", methods=["DELETE"])
+def eliminar_resultado(id):
+    config_data = load_file_config()
+    url = config_data["url-backend-registraduria"] + "/resultado/" + id
+    response = requests.delete(url)
+    return jsonify(response.json())
 
 
 if __name__ == '__main__':
